96a9331f4cfd7bf56f6b5b0b67d1a5335bcd612d
Twitty97 <farangiz.nurmukhamedova97@gmail.com>
2025-06-13 03:48:12 +0500

    Fix: updated S3 bucket name to avoid conflict

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "fara-terraform-state-bucket-2025"
+    bucket = "fara-terraform-state-bucket-2025-farangiz001"
     key    = "global/s3/terraform.tfstate"
     region = "us-east-1"
   }
